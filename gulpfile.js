5b60b71e7bc12b611848ace517d592f80dcfe46e
Kimserey Lam <kimserey.lam@gmail.com>
2016-06-15 23:18:00 +0100

    Added minify css and minify js

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
 // Sass configuration
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var minifyCs = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
 
-gulp.task('sass', function() {
+gulp.task('produce-dist', function() {
     gulp.src('./scss/SimpleUI.scss')
         .pipe(sass())
-        .pipe(gulp.dest("./css"))
+        .pipe(gulp.dest("./css"));
+
+    gulp.src('./css/SimpleUI.css')
+        .pipe(sass())
+        .pipe(gulp.dest('./dist/css'));
+
+    gulp.src('./js/SimpleUI.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
 });
